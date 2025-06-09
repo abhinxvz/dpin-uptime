c88b370f71efcb0c0ce9448c629e818c80ae8814
Abhinav Singh <abhinxv18@gmail.com>
2025-06-09 15:40:37 +0530

    xyz

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,9 +1,29 @@
 import express from "express";
+import { authMiddleware } from "./middleware";
 
 const app = express();
 
 
+app.post("/api/v1/website", authMiddleware , (req, res) => {
+const userId = req.userId;
 
+})
+
+
+app.get("/api/v1/status", authMiddleware , (req, res) => {
+
+
+})
+
+
+app.get("/api/v1/websites", authMiddleware , (req, res) => {
+
+
+})
+
+app.delete("/api/v1/website", authMiddleware , (req, res) => {
+
+})
 
 
 app.listen(3000);
